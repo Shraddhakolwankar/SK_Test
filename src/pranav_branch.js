97377cf8a505a6502ff2c470845109fb1cfe3c8b
mitesh_canwilltech <miteshkambli69@gmail.com>
2023-07-22 15:16:34 +0530

    file change

---
+++ src/pranav_branch.js
@@ -0,0 +1,17 @@
+function displayText() {
+   
+    var Sk = 'abc'
+     var text = 'SK_branchTest'
+}
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class pranav_branch2 extends Component {
+  render() {
+    return (
+      <View>
+        <Text>pranav_branch1</Text>
+      </View>
+    )
+  }
+}
